a954127dc283754360ae5a5b711c289c6001fac0
Talita <talitanieps@pecege.com>
2024-04-02 15:37:32 -0300

    Implementando rotas CRUD

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -1,8 +1,49 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
+database = []
 
-@app.get('/')
+
+@app.get('/', status_code=200, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.post('/users/', status_code=201, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+    database.append(user_with_id)
+    return user_with_id
+
+
+@app.get('/users/', response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)  # user sem senha
+def update_user(user_id: int, user: UserSchema):
+    # if user_id not in database: - é possível usar a sintaxe, mas ela fica menos clara no código
+
+    if user_id > len(database) or user_id < 0:
+        raise HTTPException(status_code=404, detail='User not Found')
+
+    user_with_id = UserDB(
+        **user.model_dump(), id=user_id
+    )   # o ** quer dizer que o dicionário será desempacotado em parâmetros
+    # UserDB(username='nome do usuario', password='senha do usuario'), etc.
+    database[user_id - 1] = user_with_id
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=404, detail='User not found')
+
+    del database[user_id - 1]
+
+    return {'message': 'User deleted'}
--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -1,16 +1,75 @@
-from fastapi.testclient import TestClient
-
-from fast_zero.app import app
+# from fast_zero.app import app
 
 
-def test_root_deve_retornar_200_e_ola_mundo():
-    client = TestClient(app) # Arrange - configurando um lcient de testes para fazer a requisição ao app
-
-    response = client.get('/') # Act - chamada do Sistema Sob teste: estamos exercitando a rota '/' e armazenando
-    #a resposta na variável response - fase que executa o código a ser testado
+def test_root_deve_retornar_200_e_ola_mundo(
+    client,
+):   # Arrange - configurando um lcient de testes para fazer a requisição ao app - resumido na função client
+    response = client.get(
+        '/'
+    )   # Act - chamada do Sistema Sob teste: estamos exercitando a rota '/' e armazenando
+    # a resposta na variável response - fase que executa o código a ser testado
 
     assert response.status_code == 200
-    assert response.json() == {'message': 'Olá Mundo!'} # Assert : verifica se tudo ocorreu como esperado
+    assert response.json() == {
+        'message': 'Olá Mundo!'
+    }   # Assert : verifica se tudo ocorreu como esperado
     #
 
 
+# Teste para a rota POST deve verificar se a criação de um novo usuário funciona corretamente; enviamos um novo usuário
+# para a rota /users/ e, em seguida, verifica a resposta é 201 (criado)
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == 201
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
+
+
+def test_read_user(client):
+    response = client.get('/users/')
+    assert response.status_code == 200   # success
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'alice',
+                'email': 'alice@example.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpass',
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == 200
+    assert response.json() == {'message': 'User deleted'}
